0911815f5dbf58f3a71855eb17cd928189e7b273
Murillo Gama <DevMurilloGama2005@gmail.com>
2024-07-04 20:23:19 -0300

    Criando a classe posicao

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Globalization;
+using tabuleiro;
+
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CultureInfo CI = CultureInfo.InvariantCulture;
+
+            posicao P;
+
+            P = new posicao(3, 4);
+
+            Console.WriteLine("Posição: " + P);
+
+            Console.WriteLine();
+        }
+    }
+}
---
+++ xadrez-console/tabuleiro/posicao.cs
@@ -0,0 +1,19 @@
+﻿namespace tabuleiro
+{
+    internal class posicao
+    {
+        public int Linha { get; set; }
+        public int Colunas { get; set; }
+
+         public posicao(int Linha, int Colunas)
+        {
+            this.Linha = Linha;
+            this.Colunas = Colunas;
+        }
+
+        public override string ToString()
+        {
+            return Linha + ", " + Colunas;
+        }
+    }
+}
